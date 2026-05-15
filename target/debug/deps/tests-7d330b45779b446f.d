eb808990c228d97384d210f78b22dd7045e5d40e
Pedro Omar <pocl9812@gmail.com>
2026-05-15 12:02:24 -0400

    add: first commit

---
+++ target/debug/deps/tests-7d330b45779b446f.d
@@ -0,0 +1,5 @@
+/Users/pocl1998/Desktop/Proyectos/Trondealer-Rust/target/debug/deps/tests-7d330b45779b446f.d: tests/tests.rs
+
+/Users/pocl1998/Desktop/Proyectos/Trondealer-Rust/target/debug/deps/libtests-7d330b45779b446f.rmeta: tests/tests.rs
+
+tests/tests.rs:
